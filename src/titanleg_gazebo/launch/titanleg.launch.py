73e2acfd1e1d39a5a5eabbf81cdd443e57801807
SKYBIRDSGP <sgpatki3422@gmail.com>
2024-08-06 10:19:48 +0530

    Adds the .sdf and the launch files

--- src/titanleg_gazebo/launch/titanleg.launch.py
+++ src/titanleg_gazebo/launch/titanleg.launch.py
@@ -3,109 +3,93 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument,  ExecuteProcess, RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, LogInfo
 from launch.substitutions import Command
 from launch_ros.actions import Node
-import launch_ros.descriptions
-from launch.event_handlers import (OnProcessStart, OnProcessExit)
-from launch.actions import IncludeLaunchDescription
+from launch.event_handlers import OnProcessExit
 from launch_ros.descriptions import ParameterValue
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_prefix
-import random
 
-# this is the function launch which the system will look for
 def generate_launch_description():
     
     ##########DATA_INPUT##########
-    urdf_file = 'URDF_Slider.urdf'
+    sdf_file = 'titanleg.sdf'
     package_description = 'titanleg_gazebo'
     ##########DATA_INPUT_END##########
-    config = os.path.join( get_package_share_directory('titanleg_gazebo'),
-    'config',
-    'titanleg.yaml'
-    )
-    robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
-    print("Searching for the Design ===>")
-    robot_description_content =  ParameterValue(Command(['xacro ', robot_desc_path]),value_type=str)
+    
+    config = os.path.join(get_package_share_directory('titanleg_gazebo'), 'config', 'titanleg.yaml')
+    robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", sdf_file)
+    
+    # Directly read the SDF content
+    with open(robot_desc_path, 'r') as sdf_file:
+        robot_description_content = sdf_file.read()
+
     robot_description = {"robot_description": robot_description_content}
     
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[robot_description, config],
-        output="both",
+        output="screen",
+        arguments=['--ros-args', '--log-level', 'debug']
     )
+    
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        output="screen",
     )
 
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["forward_position_controller", "--controller-manager", "/controller_manager"],
+        output="screen",
     )
     
-    ### Robot State Publisher
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
-        emulate_tty=True,
-        parameters=[{'use_sim_time': True, 'robot_description': ParameterValue(Command(['xacro ', robot_desc_path]),value_type=str)}],
-        output = "screen"
+        parameters=[{'use_sim_time': True, 'robot_description': robot_description_content}],
+        output="screen",
     )
-    # Position and orientaion of the Leg : 
-    # [X, Y, Z]
+    
     position = [0.0, 0.0, 0.0]
-    # [Roll, Pitch, Yaw]
     orientation = [0.0, 0.0, 0.0]
-    # base name of the bot: 
     robot_base_name = "titanleg"
-    # Spawn Leg Set Gazebo
+    
     spawn_robot = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         name='spawn_entity',
         output='screen',
         arguments=['-entity', robot_base_name,
-                   '-x', str(position[0]), '-y', str(position[1]
-                                                     ), '-z', str(position[2]),
-                    '-R', str(orientation[0]), '-P', str(orientation[1]
-                                                         ), '-Y', str(orientation[2]),
-                    '-topic', '/robot_description'                                                                     
-                   ]
+                   '-x', str(position[0]), '-y', str(position[1]), '-z', str(position[2]),
+                   '-R', str(orientation[0]), '-P', str(orientation[1]), '-Y', str(orientation[2]),
+                   '-topic', '/robot_description']
     )
 
-    # create and return launch description object
     delay_joint_state_broadcaster_spawner_after_spawn_robot = RegisterEventHandler(
         event_handler=OnProcessExit(
             target_action=spawn_robot,
-            on_exit=[joint_state_broadcaster_spawner],
+            on_exit=[LogInfo(msg="Spawning joint_state_broadcaster"), joint_state_broadcaster_spawner],
         )
     )
+    
     delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
         event_handler=OnProcessExit(
             target_action=joint_state_broadcaster_spawner,
-            on_exit=[robot_controller_spawner],
-        )
-    )
-    control_node_after_gazebo = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=spawn_robot,
-            on_exit=[control_node],
+            on_exit=[LogInfo(msg="Spawning forward_position_controller"), robot_controller_spawner],
         )
     )
 
     return LaunchDescription([
-        spawn_robot,
-        control_node_after_gazebo,
+        LogInfo(msg="Starting control node"),
+        control_node,
         robot_state_publisher_node,
+        spawn_robot,
         delay_joint_state_broadcaster_spawner_after_spawn_robot,
-        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner
-        
-        ]
-    )
-          
+        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+        LogInfo(msg="Launch file executed successfully"),
+    ])
